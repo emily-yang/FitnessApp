f187936cf89f643114e0b2368a4a2a42078b2406
Taylor Tobin <19tt94@gmail.com>
2017-11-07 10:44:27 -0800

    set up menu

--- app/src/main/java/edu/csulb/android/fitnessapp/myLogsActivity.java
+++ app/src/main/java/edu/csulb/android/fitnessapp/myLogsActivity.java
@@ -1,9 +1,29 @@
 package edu.csulb.android.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class myLogsActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_home) {
+            final Intent viewHome = new Intent(this, MainActivity.class);
+            startActivity(viewHome);
+            return true;
+        }
+        if (id == R.id.action_account) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/edu/csulb/android/fitnessapp/myWorkoutsActivity.java
+++ app/src/main/java/edu/csulb/android/fitnessapp/myWorkoutsActivity.java
@@ -1,13 +1,33 @@
 package edu.csulb.android.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class myWorkoutsActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_home) {
+            final Intent viewHome = new Intent(this, MainActivity.class);
+            startActivity(viewHome);
+            return true;
+        }
+        if (id == R.id.action_account) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout_track);
+        setContentView(R.layout.activity_workouts);
     }
 }
