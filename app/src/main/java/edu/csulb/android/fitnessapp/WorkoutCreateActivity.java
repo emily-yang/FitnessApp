e557758738fa80685c02663e9702f959fc87b205
Taylor Tobin <19tt94@gmail.com>
2017-11-07 10:46:07 -0800

    working menu

--- app/src/main/java/edu/csulb/android/fitnessapp/WorkoutCreateActivity.java
+++ app/src/main/java/edu/csulb/android/fitnessapp/WorkoutCreateActivity.java
@@ -3,9 +3,16 @@ package edu.csulb.android.fitnessapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class WorkoutCreateActivity extends AppCompatActivity {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
--- app/src/main/java/edu/csulb/android/fitnessapp/inspirationList.java
+++ app/src/main/java/edu/csulb/android/fitnessapp/inspirationList.java
@@ -3,6 +3,7 @@ package edu.csulb.android.fitnessapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +18,12 @@ import java.util.List;
 import java.util.Map;
 
 public class inspirationList extends AppCompatActivity {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
