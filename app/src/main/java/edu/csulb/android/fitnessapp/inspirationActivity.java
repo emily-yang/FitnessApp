0719de9baa08c1cb179c6f6bd6273728cba7a192
Taylor Tobin <19tt94@gmail.com>
2017-12-06 22:27:16 -0800

    set up buttons

--- app/src/main/java/edu/csulb/android/fitnessapp/inspirationActivity.java
+++ app/src/main/java/edu/csulb/android/fitnessapp/inspirationActivity.java
@@ -11,6 +11,12 @@ import android.widget.Button;
 public class inspirationActivity extends BaseActivity {
     private Button list;
 
+    private Button workout1;
+    private Button workout2;
+    private Button workout3;
+    private Button workout4;
+    private Button workout5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,45 @@ public class inspirationActivity extends BaseActivity {
             }
         });
 
+        final Intent getWorkout1 = new Intent(this, inspirationList.class);
+        workout1 = (Button)findViewById(R.id.workout1);
+        workout1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(getList);
+            }
+        });
+
+        final Intent getWorkout2 = new Intent(this, inspirationList.class);
+        workout2 = (Button)findViewById(R.id.workout2);
+        workout2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(getList);
+            }
+        });
+
+        final Intent getWorkout3 = new Intent(this, inspirationList.class);
+        workout3 = (Button)findViewById(R.id.workout3);
+        workout3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(getList);
+            }
+        });
+
+        final Intent getWorkout4 = new Intent(this, inspirationList.class);
+        workout4 = (Button)findViewById(R.id.workout4);
+        workout4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(getList);
+            }
+        });
+
+        final Intent getWorkout5 = new Intent(this, inspirationList.class);
+        workout5 = (Button)findViewById(R.id.workout5);
+        workout5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(getList);
+            }
+        });
 
     }
 }
